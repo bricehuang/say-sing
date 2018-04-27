e03e3db8489075daa5ef7610a6d9b0b89a9f4344
Brice Huang <bmhuang@mit.edu>
2018-04-26 20:36:23 -0400

    test_pitch creates test pitch dir on first run

--- test_pitch.py
+++ test_pitch.py
@@ -1,12 +1,15 @@
 from audio import produce_soundfile
 from play import SOUND_BY_PITCHES, REPS_PER_SECOND_STORE
 import wave
+import os
 
 TEST_PITCH_DIR = './test_pitch/'
 TEST_RATE = 720
 TEST_REPEATS = 100
 
 if __name__ == '__main__':
+    if not os.path.exists(TEST_PITCH_DIR):
+        os.makedirs(TEST_PITCH_DIR)
     for note, (sound, voice) in SOUND_BY_PITCHES.iteritems():
         produce_soundfile(sound*TEST_REPEATS, voice, TEST_RATE, TEST_PITCH_DIR+note+'.wav')
     def _get_repeats_per_second(note):
